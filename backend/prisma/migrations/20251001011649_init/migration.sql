38af0b0d00728001a2a84c452ba95b7667b038e7
Дмитрий Богданов <dmitry.b@digital-spectr.ru>
2025-10-09 00:01:27 +0300

    Оптимизация запроса

--- backend/prisma/migrations/20251001011649_init/migration.sql
+++ backend/prisma/migrations/20251001011649_init/migration.sql
@@ -12,14 +12,14 @@ CREATE TABLE "ideas" (
 CREATE TABLE "votes" (
     "id" TEXT NOT NULL,
     "idea_id" TEXT NOT NULL,
-    "ip" TEXT NOT NULL,
+    "ip" INET NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "votes_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "votes_idea_id_ip_key" ON "votes"("idea_id", "ip");
+CREATE UNIQUE INDEX "votes_ip_idea_id_key" ON "votes"("ip", "idea_id");
 
 -- AddForeignKey
 ALTER TABLE "votes" ADD CONSTRAINT "votes_idea_id_fkey" FOREIGN KEY ("idea_id") REFERENCES "ideas"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
